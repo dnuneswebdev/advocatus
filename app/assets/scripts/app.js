80419a0a6d0e91e253fe4e2bc08e8b87f293a6c5
Daniel <dnuneswebdev@gmail.com>
2017-11-20 17:01:56 -0200

    Added reveal items on scroll and some hover css effects

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,12 @@
+import $ from "jquery";
 import MobileMenu from "./modules/MobileMenu";
 import StickyHeader from "./modules/StickyHeader";
-
+import RevealOnScroll from "./modules/RevealOnScroll";
 
 
 
 var mobileMenu = new MobileMenu();
 var stickyHeader = new StickyHeader();
+new RevealOnScroll($(".lawyers"), "75%");
+new RevealOnScroll($(".practice-item"), "100%");
+new RevealOnScroll($(".contact"), "100%");
