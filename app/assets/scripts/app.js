a53913bfa5f53214ff4e45c84204335791478f0c
Daniel <dnuneswebdev@gmail.com>
2017-11-20 18:06:11 -0200

    Finished modal and changed some details

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 import MobileMenu from "./modules/MobileMenu";
 import StickyHeader from "./modules/StickyHeader";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import Modal from "./modules/Modal";
 
 
 
@@ -10,3 +11,4 @@ var stickyHeader = new StickyHeader();
 new RevealOnScroll($(".lawyers"), "75%");
 new RevealOnScroll($(".practice-item"), "100%");
 new RevealOnScroll($(".contact"), "100%");
+var modal = new Modal();
